0309819a36c2001cd5262eeb3cfca72c4b8b37a4
loukq <loukq_offic@163.com>
2019-08-01 18:00:05 +0800

    [ADD] 增加配置文件

--- platform-main/src/main/java/org/platform/main/ApplicationServerMain.java
+++ platform-main/src/main/java/org/platform/main/ApplicationServerMain.java
@@ -2,11 +2,13 @@ package org.platform.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 /**
  * @Author lou
  */
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.platform.server.*"})
+@ImportResource(locations={"classpath:application.xml"})
 public class ApplicationServerMain {
 	public static void main(String[] args) {
 		SpringApplication.run(ApplicationServerMain.class, args);
