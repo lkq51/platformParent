ca1516fc52353306db0b6a6e2fced8cc1738db72
loukq <loukq_offic@163.com>
2019-08-01 17:46:13 +0800

    [MOD] 常规修改

--- utils/src/test/java/org/platform/utils/ListTest.java
+++ utils/src/test/java/org/platform/utils/ListTest.java
@@ -1,16 +1,22 @@
 package org.platform.utils;
 
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * @Author lou
  */
 public class ListTest {
 	public static void main(String[] args) {
-		Thing t = new Thing();
-		t.getData().add("1");
-		System.out.println(t.getData());
+		HashMap<String, String> map = new HashMap<String, String>();
+
+		map.put("1", "2");
+		map.put("1", "1");
+
+		System.out.println(map.get("1"));
 	}
 
 	static class Thing{
--- utils/src/test/scala/com/platform/Test.scala
+++ utils/src/test/scala/com/platform/Test.scala
@@ -1,5 +1,6 @@
 package com.platform
 
+import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
 import scala.collection.mutable.ArrayBuffer
@@ -12,15 +13,15 @@ object Test {
 			val sc: SparkContext = new SparkContext(conf)
 
 			val str1 = new ArrayBuffer[String]()
-			str1 += "one-1-abc"
-			str1 += "two-2-abc"
-			str1 += "three-3-abc"
-			str1 += "three-3-abc"
-			str1 += "three-3-abc"
-			str1 += "three-4-abc"
+			str1 += "one-1-2"
+			str1 += "two-2-3"
+			str1 += "three-1-1"
+			str1 += "three-1-2"
+			str1 += "three-1-3"
+			str1 += "three-4-4"
 
-			val words = sc.parallelize(str1).map(x => {
-				(x.split("-")(0), x.split("-")(1), x.split("-")(2))
+			val words:RDD[(String, String, Int)] = sc.parallelize(str1).map(x => {
+				(x.split("-")(0), x.split("-")(1), x.split("-")(2).toInt)
 			})
 			words.foreach(println)
 			val wordSum = words.map(res => (res._1, 1)).reduceByKey(_ + _)
@@ -38,7 +39,18 @@ object Test {
 			}).map(x => {
 				x._2._1._2
 			}).flatMap( x => x)
+		println("result的内容")
 		result.foreach(println)
+
+		val result1 = result.map(x => {
+			(x._1, List(x))
+		}).reduceByKey((x, y) => {
+			(x ++:y).sortBy(_._3)
+		}).filter(res => {
+			res._2.nonEmpty
+		})
+		println("result1 的内容")
+		result1.foreach(println)
 	}
 
 }
