bc7a9439a1a5b78333e419b0ea0716522ee45da0
lou <626883409@qq.com>
2019-08-02 00:31:21 +0800

    [MOD] 完成了kafka producer demo

--- platform-main/src/main/java/org/platform/main/ApplicationServerMain.java
+++ platform-main/src/main/java/org/platform/main/ApplicationServerMain.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.ImportResource;
  * @Author lou
  */
 @SpringBootApplication(scanBasePackages = {"com.platform.server.*"})
-@ImportResource(locations={"classpath:application.xml"})
+//@ImportResource(locations={"classpath:application.properties"})
 public class ApplicationServerMain {
 	public static void main(String[] args) {
 		SpringApplication.run(ApplicationServerMain.class, args);
--- utils/src/test/java/org/platform/kafka/KafkaProducerDemo.java
+++ utils/src/test/java/org/platform/kafka/KafkaProducerDemo.java
@@ -1,12 +1,12 @@
 package org.platform.kafka;
 
+import com.alibaba.fastjson.JSONObject;
 import kafka.javaapi.producer.Producer;
+import kafka.producer.KeyedMessage;
 import kafka.producer.ProducerConfig;
 import kafka.serializer.StringDecoder;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
+import java.util.*;
 
 /**
  * @Date 2019-07-31
@@ -32,4 +32,30 @@ public class KafkaProducerDemo extends Thread {
 		return new Producer<Integer, String>(new ProducerConfig(properties));
 	}
 
+	@Override
+	public void run() {
+		System.out.println("start to send message to kafka");
+		int count = 0;
+		while (true){
+			JSONObject jsonObject = new JSONObject();
+			int num = new Random().nextInt(10);
+			String ip = "ip" + num;
+			String userId = UUID.randomUUID().toString().replace("-", "");
+			jsonObject.put("userId", userId);
+			jsonObject.put("userName", "Tiger" + userId);
+			System.out.println("userId >>> " + userId);
+			producer.send(new KeyedMessage<String, String>(topic, userId, jsonObject.toJSONString()));
+
+			try {
+				Thread.sleep(10);
+			}catch (InterruptedException e){
+				e.printStackTrace();
+			}
+			count ++;
+			if (count == 500){
+				System.out.println("stop to send message to kafka");
+				break;
+			}
+		}
+	}
 }
